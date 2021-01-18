fcd7f1002cf8882b17e7d2a25798bb910a3b8098
minefarmer <pgoldfarm@yahoo.com>
2021-01-18 11:46:35 -0600

    bye

--- LessonNotes/CuidedExercises/If.py
+++ LessonNotes/CuidedExercises/If.py
@@ -16,3 +16,46 @@
 >>> reaction("among us")
 'yay!'
 >>> 
+
+
+
+def gt5o(x):
+	if x > 5:
+		return "yay!"
+	else:
+		return "nu!"
+
+ gt5o(10)
+'yay!'
+ gt5o(2)
+'nu!'
+ 
+
+
+
+def reaction(game):
+	if game == "among us":
+		return "yay!"
+	else:
+		return "nu!"
+
+ reaction("among us")
+'yay!'
+ reaction("tic tack toe")
+'nu!'
+ 
+
+
+
+def blackjack(lst):
+	if sum(lst) < 21:
+		return sum(lst)
+	else:
+		return 0
+
+ blackjack([20,20])
+0
+ blackjack([3, 4, 5])
+12
+ 
+
